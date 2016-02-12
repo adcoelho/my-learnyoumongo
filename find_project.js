0f95646a37b9bd079c473caf4684343c72ba229f
adcoelho <antoniodcoelho@gmail.com>
2016-02-12 16:22:06 +0000

    Solved Find Project exercise

--- find_project.js
+++ find_project.js
@@ -5,16 +5,26 @@ var age = process.argv[2];
 /*
 Use the parrots collection to find all documents where age
 is greater than the first argument passed to your script.
+
+The difference from the last lesson will be that we only want the
+name and age properties
+
 Using console.log, print the documents to stdout.
 */
+
 MongoClient.connect(db_url, function (err, db) {
-    if (err) return err;
+    if (err) throw err;
     var collection = db.collection('parrots');
     collection.find({
         age: { $gt: +age }
-    }).toArray(function (err, doc) {
+    },
+    {
+        name: 1,
+        age: 1,
+        _id: 0
+    }).toArray(function (err, docs) {
         if (err) throw err;
-        console.log(doc);
-        db.close();
-    });
-});
+        console.log(docs);
+        db.end();
+    })
+})
