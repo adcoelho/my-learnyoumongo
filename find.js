823d358c42bb546d860868ce2dad9aa7a92e64d0
adcoelho <antoniodcoelho@gmail.com>
2016-02-12 15:38:58 +0000

    Solved find exercise

---
+++ find.js
@@ -0,0 +1,19 @@
+var MongoClient = require('mongodb').MongoClient;
+var db_url = 'mongodb://localhost:27017/learnyoumongo';
+var age = process.argv[2];
+/*
+Use the parrots collection to find all documents where age
+is greater than the first argument passed to your script.
+Using console.log, print the documents to stdout.
+*/
+MongoClient.connect(db_url, function (err, db) {
+    if (err) return err;
+    var collection = db.collection('parrots');
+    collection.find({
+        age: { $gt: +age }
+    }).toArray(function (err, doc) {
+        if (err) throw err;
+        console.log(doc);
+        db.close();
+    });
+});
